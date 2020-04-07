b2b93abe5782a137205b1a82ae74511eb4a87b15
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-06 18:30:04 -0700

    Created Engineer and intern modules

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,12 +4,12 @@ constructor(name, id, email){
 this.name = name;
 this.id = id;
 this.email = email;
-
+this.role = "Employee"
 }
 getName() {return this.name}
 getId() {return this.id}
 getEmail() {return this.email}
-getRole() {return "Employee"}
+getRole() {return this.role}
 
 }
 
--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -1,18 +1,18 @@
 const Engineer = require("../lib/Engineer");
 
-test.skip("Can set GitHUb account via constructor", () => {
+test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
-test.skip("getRole() should return \"Engineer\"", () => {
+test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
-test.skip("Can get GitHub username via getGithub()", () => {
+test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
