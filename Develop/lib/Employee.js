b9b402ed7b5645ec65ef0652add28153a4941da9
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-06 18:14:46 -0700

    Created employee module

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,9 +1,16 @@
 // TODO: Write code to define and export the Employee class
 class Employee{
-constructor(){
-
+constructor(name, id, email){
+this.name = name;
+this.id = id;
+this.email = email;
 
 }
+getName() {return this.name}
+getId() {return this.id}
+getEmail() {return this.email}
+getRole() {return "Employee"}
+
 }
 
 module.exports = Employee;
