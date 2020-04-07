52505b1b179d8b2943d80df425e6da9d6fb35909
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-06 18:32:42 -0700

    Add maager module

--- Develop/test/Manager.test.js
+++ Develop/test/Manager.test.js
@@ -1,19 +1,19 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-test.skip("Can set office number via constructor argument", () => {
+test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
-test.skip('getRole() should return "Manager"', () => {
+test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
 
-test.skip("Can get office number via getOffice()", () => {
+test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
